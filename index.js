f946e6ac86f9e3a713c7ed96897fd8b1fb5069e2
Daniel Trone <royhobbstn@yahoo.com>
2017-04-30 16:29:36 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+//redirect http to https
+
+// http://heyrod.com/snippets/redirect-http-to-https-in-expressjs.html
+var http = require('http');
+var express = require('express');
+var HTTP_PORT = 80;
+var HTTPS_PORT = 443;
+
+var http_app = express();
+http_app.set('port', HTTP_PORT);
+
+
+http_app.all('/*', function (req, res, next) {
+    if (/^http$/.test(req.protocol)) {
+        var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any
+        if ((HTTPS_PORT != null) && HTTPS_PORT !== 443) {
+            return res.redirect("https://" + host + ":" + HTTPS_PORT + req.url, 301);
+        }
+        else {
+            return res.redirect("https://" + host + req.url, 301);
+        }
+    }
+    else {
+        return next();
+    }
+});
+
+http.createServer(http_app).listen(HTTP_PORT).on('listening', function () {
+    return console.log("HTTP to HTTPS redirect app launched.");
+});
+
+
+
+// Redbird
+
+
+var sslobj = {
+    port: 443,
+    key: 'ssl/docker/privkey1.pem',
+    cert: 'ssl/docker/fullchain1.pem',
+    ca: 'ssl/docker/chain1.pem'
+};
+
+
+var redbird = require('redbird')({
+    port: 443,
+    xfwd: false,
+    ssl: sslobj
+});
+
+
+redbird.register('tiles.red-meteor.com/mbtiles', 'http://red-meteor.com:4001', {
+    ssl: true
+});
